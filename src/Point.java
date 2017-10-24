0e56b9880f96ad6735ba24aa00b35c5414387914
Kuznetsov Pavel <Pavel.Sergeevich.K@yandex.ru>
2017-10-24 17:04:52 +0300

    add isLeft operation

--- src/Point.java
+++ src/Point.java
@@ -2,8 +2,8 @@ import java.math.BigInteger;
 
 class Point {
 
-    final private BigInteger x;
-    final private BigInteger y;
+    private final BigInteger x;
+    private final BigInteger y;
 
     Point(final BigInteger x, final BigInteger y) {
         this.x = x;
@@ -13,5 +13,31 @@ class Point {
     static Point parsePoint(final String s) {
         String[] array = s.split(" ");
         return new Point(new BigInteger(array[0]), new BigInteger(array[1]));
+    }
+
+    boolean isLeftOfLine(final Point from, final Point to) {
+        return calcCrossProduct(from, to).compareTo(new BigInteger("0")) > 0;
+    }
+
+    private BigInteger calcCrossProduct(final Point p1, final Point p2) {
+        return ((p2.x.subtract(p1.x)).multiply(this.y.subtract(p1.y)))
+                .subtract((p2.y.subtract(p1.y)).multiply(this.x.subtract(p1.x)));
+    }
+
+    public BigInteger getX() {
+        return x;
+    }
+
+    public BigInteger getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point) {
+            if (x.compareTo(((Point) obj).x) == 0 && y.compareTo(((Point) obj).y) == 0)
+                return true;
+        }
+        return false;
     }
 }
