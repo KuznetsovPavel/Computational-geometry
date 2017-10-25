5ef56bf20ea4198da4319601f5c4b591e98b5d99
Kuznetsov Pavel <Pavel.Sergeevich.K@yandex.ru>
2017-10-26 01:57:39 +0300

    update test performance

--- src/test/TestPerformance.java
+++ src/test/TestPerformance.java
@@ -14,20 +14,23 @@ public class TestPerformance {
         final int BITS = 12;
         final int COUNT = 10;
         Random random = new Random();
-        for (int i = 1; i < 10; i++) {
-            ArrayList<Point> points = new ArrayList<>();
+        for (int i = 1; i < 15; i++) {
+            long sum = 0;
             final int currentSize = SIZE * i;
-            for (int j = 0; j < currentSize; j++) {
-                points.add(new Point(new BigInteger(BITS, random), new BigInteger(BITS, random)));
-            }
-            final long start = System.currentTimeMillis();
             for (int j = 0; j < COUNT; j++) {
+                ArrayList<Point> points = new ArrayList<>();
+                for (int k = 0; k < currentSize; k++) {
+                    points.add(new Point(new BigInteger(BITS, random), new BigInteger(BITS, random)));
+                }
+                final long start = System.currentTimeMillis();
                 ArrayList<Integer> result = (ArrayList<Integer>) new QuickHull(points).execute();
+                final long finish = System.currentTimeMillis();
+                final long elapsed = (finish - start);
+                sum += elapsed;
             }
-            final long finish = System.currentTimeMillis();
-            final long elapsed = (finish - start)/COUNT;
-            System.out.format("for %d elements: %4d msec. Perform: %f \n", currentSize, elapsed,
-                    (currentSize * Math.log(currentSize) / elapsed));
+            sum /= COUNT;
+            System.out.format("for %6d elements: %5d msec. Perform: %f \n", currentSize, sum,
+                    (currentSize * Math.log(currentSize) / sum));
         }
     }
 }
